0a6f152edc463a05d48d07878dcc5780293deb99
Sumaya <sumayajaabo07@gmail.com>
2024-11-02 15:09:15 +0300

    pot item

--- src/app/pots/pots-overview/pots-overview.component.ts
+++ src/app/pots/pots-overview/pots-overview.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PotItemComponent } from '../pot-item/pot-item.component';
 
 @Component({
   selector: 'app-pots-overview',
   standalone: true,
-  imports: [],
+  imports: [PotItemComponent],
   templateUrl: './pots-overview.component.html',
   styleUrl: './pots-overview.component.css'
 })
